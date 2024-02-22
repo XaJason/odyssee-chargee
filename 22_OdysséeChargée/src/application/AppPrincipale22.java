b207ec7fcefcc925649567cd2ad09ab89e84efce
Kitimir Yim <kitimir.yim@gmail.com>
2024-02-22 17:19:24 -0500

    Ajout de quelques classes

--- 22_OdysséeChargée/src/application/AppPrincipale22.java
+++ 22_OdysséeChargée/src/application/AppPrincipale22.java
@@ -1,5 +1,40 @@
 package application;
 
-public class AppPrincipale22 {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class AppPrincipale22 extends JFrame{
+    /**
+     * Zone des composants
+     */
+    private JPanel contentPane;
+
+	/**
+	 * Lance l'application
+	 * @param args Paramètre d'entrée de la commande de démarrage
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppPrincipale22 frame = new AppPrincipale22();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public AppPrincipale22(){
+		setTitle("Odyssée Chargée");
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
 }
---
+++ 22_OdysséeChargée/src/fenetres/FenetreApropos.java
@@ -0,0 +1,13 @@
+package fenetres;
+
+import javax.swing.JFrame;
+
+public class FenetreApropos extends JFrame {
+
+	public FenetreApropos() {
+		setTitle("À propos");
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+	}
+
+}
