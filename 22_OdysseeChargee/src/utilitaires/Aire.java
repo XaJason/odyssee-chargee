dc759186bc2119cbc57b92079b50f6354ec24d7d
Jason Xa <jasonxahainam@hotmail.com>
2024-04-02 16:19:56 -0400

    Ajout de la propriété pointMilieu à Aire pour l'intégration des plaques

--- 22_OdysseeChargee/src/utilitaires/Aire.java
+++ 22_OdysseeChargee/src/utilitaires/Aire.java
@@ -4,19 +4,26 @@ import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 
 /**
+ * Un objet Aire représente une aire triangulaire intérieure d'une tuile qui
+ * peut porter une plaque chargée. Une tuile a autant d'aires que de côtés.
  * 
  * @author Jason Xa
  */
 public class Aire {
-	/***/
+	/** le triangle formé par les trois points */
 	private Path2D.Double forme;
-	/***/
+	/**
+	 * les points extérieurs, les points qui sont adjacents aux bordures de la tuile
+	 */
 	private Point2D.Double[] pointsExterieurs;
 
+	private Point2D.Double pointMilieu;
+
 	/**
-	 * @param point1
-	 * @param pointMilieu
-	 * @param point2
+	 * @param point1      le premier point extérieur
+	 * @param pointMilieu le point milieu, le point qui se situe à l'intérieur de la
+	 *                    tuile
+	 * @param point2      le deuxième point extérieur
 	 * 
 	 */
 	// Jason Xa
@@ -28,6 +35,8 @@ public class Aire {
 		forme.closePath();
 
 		pointsExterieurs = new Point2D.Double[] { point1, point2 };
+
+		this.pointMilieu = pointMilieu;
 	}
 
 }
